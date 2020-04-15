bae027b7e681d74c26243b33d6aebb548f21b59a
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-15 12:02:38 +0300

    1st commit

---
+++ src/main/java/org/dev/fhhf/hulkstore/HulkStoreApplication.java
@@ -0,0 +1,13 @@
+package org.dev.fhhf.hulkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev/fhhf/hulkstore/HulkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.fhhf.hulkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
