d395d49c27d73dba68e508faf6ea5d4eb0cf68af
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-15 12:14:25 +0300

    initial test data

---
+++ src/main/resources/db/hsqldb/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO products VALUES (1, 'Camiseta', 'Marvle', 'Hulk', 50);
+INSERT INTO products VALUES (2, 'Camiseta', 'Marvle', 'IronMan', 30);
+INSERT INTO products VALUES (3, 'Gorra', 'DC', 'Batman', 40);
+INSERT INTO products VALUES (4, 'Gafas', 'DC', 'WonderWoman', 25);
+INSERT INTO products VALUES (5, 'Labial', 'Marvle', 'BlackWidow', 60);
+INSERT INTO products VALUES (6, 'Bolso', 'DC', 'HarleyQuin', 55);
+
+INSERT INTO move_types VALUES (1, 'Input');
+INSERT INTO move_types VALUES (2, 'Output');
+
+INSERT INTO employees VALUES (1, 'Susana');
+INSERT INTO employees VALUES (2, 'Pedro');
+INSERT INTO employees VALUES (3, 'Carlos');
+INSERT INTO employees VALUES (4, 'Marta');
+INSERT INTO employees VALUES (5, 'Viviana');
+INSERT INTO employees VALUES (6, 'Dario');
+
+INSERT INTO movements VALUES (1, 5, '2020-01-07', 1, 1);
+INSERT INTO movements VALUES (2, 15, '2020-01-17', 2, 2);
+
+INSERT INTO product_movement VALUES (1, 2);
+INSERT INTO product_movement VALUES (1, 1);
+INSERT INTO product_movement VALUES (2, 1);
+INSERT INTO product_movement VALUES (2, 2);
