0281d90934fbdf1046adb175c68287722f031f79
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-16 23:14:17 +0300

    comprar y vender articulos

--- src/main/java/org/dev/fhhf/hulkstore/controller/MovementController.java
+++ src/main/java/org/dev/fhhf/hulkstore/controller/MovementController.java
@@ -1,6 +1,7 @@
 package org.dev.fhhf.hulkstore.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -32,18 +33,32 @@ public class MovementController {
 	@Autowired
 	private DateFormaterService dateService;
 	
-	@GetMapping("/all")
-	public String getAllInputMoves(Model model) {
-		System.out.println(moveRepo.findAll());
-		model.addAttribute("moves", moveRepo.findAll());
+	@GetMapping("/{empId}/all")
+	public String getAllInputMoves(@PathVariable("empId") int empId, Model model) {
+		List<Movement> movements = moveRepo.findAll();
+		List<List<String>> transactions = new ArrayList<>();
+		for(Movement m : movements) {
+			transactions.add(Arrays.asList(m.getMovedUnits().split(",")));
+		}
+		model.addAttribute("transactions", transactions);
+		model.addAttribute("moves", movements);
 		return "moves";
 	}
-	@GetMapping("/{empId}/initAdd")
-	public String initAddStock(@PathVariable("empId") int empId, Model model) {
+	
+	/**
+	 * Prepara el modelo para iniciar una operacion
+	 * @param empId
+	 * @param type
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/{empId}/init/{moveType}")
+	public String initAddStock(@PathVariable("empId") int empId,
+							   @PathVariable("moveType") String type, Model model) {
 		
 		Date date = dateService.giveFormat(new Date());		
 		Employee employee = empRepo.findById(empId).get();
-		Movement movement = new Movement(date, "Input", employee);
+		Movement movement = new Movement(date, type, employee);
 		
 		List<Product> products = new ArrayList<>();
 		
@@ -56,33 +71,46 @@ public class MovementController {
 		
 		model.addAttribute("movement", movement);
 		model.addAttribute("empId", empId);
+		
 		return "addStock";
 	}
 	
-	@PostMapping("/{empId}/addProducts")
-	public String executeInput(@PathVariable("empId") int empId, Movement movement) {
+	@PostMapping("/{empId}/{moveType}/Products")
+	public String executeInput(@PathVariable("empId") int empId,
+							   @PathVariable("moveType") String type, Movement movement) {
 
 		List<Product> addedProducts = (List<Product>) movement.getProducts();
 		String movedUnits = "";
 		movement.setProducts(new ArrayList<Product>());
 		
-		System.out.println(addedProducts);
-		
 		for(Product aP : addedProducts){
 			if(aP.getUnits() != 0) {
 				Product pro = productRepo.findById(aP.getId()).get();
 				
-				movedUnits = movedUnits.concat(pro.getId() +" "+ pro.getUnits() +" "+ aP.getUnits());
+				movedUnits = movedUnits.concat(pro.getId() + " _ _ _ " +
+											   pro.getUnits() + " _ _ _ " + 
+											   aP.getUnits() + ",");
+				int units = 0;
 				
-				int units = pro.getUnits() + aP.getUnits();
+				if(type.equals("Input")) {
+					units = pro.getUnits() + aP.getUnits();
+				}else
+				if(type.equals("Output")) {
+					units = pro.getUnits() - aP.getUnits();
+					if(units <= 0) {
+						return "redirect:/move/" + empId + "/all";
+					}
+				}
+				
 				pro.setUnits(units);
 				movement.addProduct(pro);
 			}
 		}
-		//System.out.println(movement.getProducts());
+		
 		movement.setMovedUnits(movedUnits);
+		
 		moveRepo.save(movement);
-		return "redirect:/move/all";
+		return "redirect:/move/" + empId + "/all";
 	}
 	
 }
