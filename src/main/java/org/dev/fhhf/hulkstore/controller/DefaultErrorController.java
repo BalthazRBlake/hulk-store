e8f11b056c30c3b70e9160d006222c3d4384d8dd
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-17 13:22:10 +0300

    cleaning code

--- src/main/java/org/dev/fhhf/hulkstore/controller/DefaultErrorController.java
+++ src/main/java/org/dev/fhhf/hulkstore/controller/DefaultErrorController.java
@@ -21,18 +21,17 @@ public class DefaultErrorController implements ErrorController{
 	@RequestMapping("/error")
 	public String handleError(HttpServletRequest request, Model model) {
 	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	    AppError error = new AppError(); 
+	    AppError error; 
+	    
 	    if (status != null) {
 	        Integer statusCode = Integer.valueOf(status.toString());
 	     
 	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            error.setErrorCode("404");
-	            error.setErrorMessage("¡La dirección a la que navegaste no existe!");
+	            error = new AppError("¡La dirección a la que navegaste no existe!", "404");
 	            model.addAttribute("error", error);
 	        }
 	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	        	error.setErrorCode("500");
-	            error.setErrorMessage("¡Lo sentimos, algo salió mal!");
+	        	error = new AppError("¡Lo sentimos, algo salió mal!", "500");
 	            model.addAttribute("error", error);
 	        }
 	    }
